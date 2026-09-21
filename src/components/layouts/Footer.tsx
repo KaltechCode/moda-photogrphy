7c0ba02e467168e774e1868bcb99eeb7eaf377c4
kaltechCode <kaltechconsultancy@gmail.com>
2026-09-21 08:54:46 -0500

    Autodeploy test 2

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -135,7 +135,7 @@ export default function Footer({ whiteFooter = false, topCls = "" }: IProps) {
             <div className="footer-box-two tp-copyright-2-left text-center text-lg-start">
               <p>
                 <Link href={"/"} className="">
-                  Designed by Kaltech Team
+                  Designed by Kaltech Consultancy
                 </Link>
               </p>
             </div>
