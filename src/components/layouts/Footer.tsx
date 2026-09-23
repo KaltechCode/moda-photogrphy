963ee0ad58f5520d4e0036d23b5013d97059a3d1
kaltechCode <kaltechconsultancy@gmail.com>
2026-09-23 01:04:09 -0500

    footer change

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -135,7 +135,7 @@ export default function Footer({ whiteFooter = false, topCls = "" }: IProps) {
             <div className="footer-box-two tp-copyright-2-left text-center text-lg-start">
               <p>
                 <Link href={"/"} className="">
-                  Designed by Kaltech Team
+                  Designed by Kaltech Team 2026
                 </Link>
               </p>
             </div>
